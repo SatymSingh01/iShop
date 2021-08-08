2650ef6a194afd02eb1d11cc6b1a689074fd42eb
melissa-correia <melissa.correia97@gmail.com>
2021-08-08 19:06:40 +0530

    added remaining classes

---
+++ src/app/Models/cart.ts
@@ -0,0 +1,7 @@
+export class Cart {
+    cart_id !: number; 
+	product_id !: number; 
+	cartproduct_quantity !: number; 
+	customer_id !: number; 
+
+}
---
+++ src/app/Models/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    product_id !: number; 
+	product_name !: string; 
+	product_imagesrc !: string; 
+	product_description !: string; 
+	product_price!:number; 
+	category_id !: number;
+	product_brand !: string;
+	retailer_id !: number; 
+	product_quantity !: number; 
+
+}
