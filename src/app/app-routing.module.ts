2f1ec933c80fa98a8d8389f9964c8de9c2ace915
SatyamSingh01 <Satyamsinghsss0101@gmail.com>
2021-08-10 20:01:19 +0530

    nav,prod,prodlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductComponent } from './product/product.component';
+// import { ProductComponent } from './product/product.component';
+// import { ProductlistComponent } from './productlist/productlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'productlist', component: ProductlistComponent},
+  {path:'', redirectTo:'productlist',pathMatch:'full'},
+  {path:'product', component: ProductComponent}
+  
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { ProductlistComponent } from './productlist/productlist.component';
+// import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule
   ],
   providers: [],
