92f891bd9e2eb1de4f302f48146bbd19a5c2ddce
SatyamSingh01 <Satyamsinghsss0101@gmail.com>
2021-08-13 14:56:36 +0530

    working code

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -30,19 +30,17 @@ export class ProductComponent implements OnInit {
     this.productservice.getById(this.router.snapshot.params['productid']).subscribe((data)=>{
       console.log(data)
       this.product=data});
+
+      this.customerservice.getById(1).subscribe(data=>{
+        this.customer=data
+      console.log(data)});
       
     
   
   }
 
   addtocart(productid:number)
-  {      
-    this.customerservice.getById(1).subscribe(data=>{
-      this.customer=data
-    console.log(data)});
-
-    
-    
+  { this.ngOnInit()  
     console.log(this.cart);
     if(this.customer.cart.length===0)
     {
@@ -56,6 +54,7 @@ export class ProductComponent implements OnInit {
           this.cart = {cartId:element.cartId,productId:productid,cartproductQuantity:element.cartproductQuantity+this.quantity,customerId:1}
           
           this.cartservice.update(element.cartId,this.cart).subscribe();
+          console.log("same products")
         }
         else{
           this.cart = {productId:productid,cartproductQuantity:this.quantity,customerId:1}
