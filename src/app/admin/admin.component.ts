b85263a795c0c1c631862d1c4c7f1e2b5b00949c
melissa-correia <melissa.correia97@gmail.com>
2021-08-14 18:48:01 +0530

    data from db displayed on admin dashboard

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Retailer } from '../Models/retailer';
+import { RetailerService } from '../Services/retailer.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  retailers: Retailer[] = [];
+  constructor(private addretailer:RetailerService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.addretailer.getAll().subscribe((data: Retailer[])=>{
+      console.log(data)
+      this.retailers= data;
+  })  
   }
 
 }
