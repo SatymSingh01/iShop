bdea945378a8124893f300c4c93651046a32d26f
melissa-correia <melissa.correia97@gmail.com>
2021-08-16 00:43:00 +0530

    alert and authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AlertService } from '../Services/alert.service';
+import { AuthenticationService } from '../Services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +12,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   submitted = false;
+  loading = false;
+
   CustomerLogin!:FormGroup;
   RetailerLogin !:FormGroup;
-  userType!:string
- 
-  constructor(private formBuilder: FormBuilder,private router:ActivatedRoute,private rou:Router ) { }
+  userType!:string;
+  returnUrl!: string;
+   
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router:ActivatedRoute,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService,
+    private rou:Router ) { }
  // constructor(private service:ApiCallService,private router:ActivatedRoute,private rou:Router) { }
 
   ngOnInit(): void {
+    localStorage.removeItem('currentUser');
     this.userType = this.router.snapshot.params['usertype']
     this.CustomerLogin = this.formBuilder.group(
       {
@@ -44,24 +57,76 @@ export class LoginComponent implements OnInit {
        
       },
     );
+
+    //  // reset login status
+    //  this.authenticationService.logout();
+    // // get return url from route parameters or default to '/'
+    //  this.returnUrl = this.router.snapshot.queryParams['returnUrl'] || '/'; //activated router
   }
 
+  // convenience getter for easy access to form fields
   get f(): { [key: string]: AbstractControl } {
     return this.CustomerLogin.controls;
   }
+
   get g(): { [key: string]: AbstractControl } {
     return this.RetailerLogin.controls;
   }
   
 
   onSubmit(): void {
+
+    if(this.userType ==='1'){
+
+    
+
     this.submitted = true;
 
+    // stop here if form is invalid
     if (this.CustomerLogin.invalid) {
+      console.log("invalid user")
       return;
     }
+    this.loading = true;//(method) AuthenticationService.login(username: string, password: string, usertype: string): Observable<any>
+        this.authenticationService.login(this.f.customerEmail.value, this.f.customerPassword.value)
+        .pipe(first())
+            .subscribe(
+                data => {
+                  console.log()
+                    this.rou.navigate(['/productlist']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                    console.log('errror')
+                });
 
      console.log(JSON.stringify(this.CustomerLogin.value, null, 2));
+              
+    }
+
+    else{
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.RetailerLogin.invalid) {
+        return;
+      }
+      this.loading = true;//(method) AuthenticationService.login(username: string, password: string, usertype: string): Observable<any>
+          this.authenticationService.login(this.g.retailerEmail.value, this.g.retailerPassword.value)
+              .pipe(first())
+              .subscribe(
+                  data => {
+                      this.rou.navigate([this.returnUrl]);
+                  },
+                  error => {
+                      this.alertService.error(error);
+                      this.loading = false;
+                  });
+  
+       console.log(JSON.stringify(this.RetailerLogin.value, null, 2));
+
+    }
   }
   }
 
